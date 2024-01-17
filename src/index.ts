212a322c2e7c58e2fbdd983fcc862f90abdb7e04
Vini <126819626+vin1i@users.noreply.github.com>
2024-01-17 20:29:56 -0300

    First_Project_TypeScript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+function component() {
+    const element = document.getElementById('content');
+
+    element.innerHTML = "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+    element.innerHTML += "<h1>Ola Mundo</h1>"
+
+    return element;
+  }
+
+component();
