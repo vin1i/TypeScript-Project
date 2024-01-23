c21d20cb3d75280009f2ce2b0c89cd76d9983161
Vini <126819626+vin1i@users.noreply.github.com>
2024-01-23 13:45:19 -0300

    Adição de funções e parametros

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,114 @@
+import { accessOptions, userType } from "./models";
+
+let content = document.getElementById('content') as HTMLInputElement;
 const button  = <HTMLInputElement>document.querySelector('button[id="add"]');
 const accessRadio  = <HTMLElement>document.getElementById('accessRadio');
 button.addEventListener('click', addEmployee );
 
 
-enum accessOptions{
-  administrator = "administrador",
-  manager = "gerente",
-  employee = "funcionário"
-}
 
 const accessOptionsValues = Object.values(accessOptions)
 
-accessOptionsValues.forEach((value: string, i: number) => {
-accessRadio.innerHTML += `
-<div class="form-check">
-<input class="form-check-input " type="radio" name="access" id="accessRadio${i}" value="${value}">
-<label class="form-check-label capitalLetter"  for="no">
-${value}
-</label>
-</div>`
-})
+/*--------------Função por expressão--------------------------------*/ 
+
+const getUser = async (): Promise<userType[]> => {
+  const response: Response= await fetch('http://localhost:5011/users');
+  const users: userType[] = await response.json();
+  return users;
+}
 
 
 
-function addEmployee(): void {
-  let content = document.getElementById('content') as HTMLInputElement;
+
+const updateUserLayout = async (): Promise<void> =>{
+  const users: userType[] = await getUser();
   
-  let fullName: HTMLInputElement | null = document.querySelector('#fullName');
-  let register = <HTMLInputElement>document.querySelector('#register');
-  let admin = document.querySelector('input[name="access"]:checked') as HTMLInputElement;
-  let active = document.querySelector('#active') as HTMLInputElement;
+  
+  users.map((user: userType) => {
+    content.innerHTML += <string>createLine(user);
+  });
+};
+/*--------------Função por expressão--------------------------------*/ 
 
- content.innerHTML += <string>(
-  createLine(fullName!.value, register.value, admin.value, active.checked)
- );
+
+
+
+updateUserLayout()
+
+  /*--------------Função declarativa--------------------------------*/ 
+function addEmployee(): void {
+  let formFields = [
+  <HTMLInputElement>document.querySelector('#fullName'),
+  <HTMLInputElement>document.querySelector('#register'),
+  document.querySelector('input[name="access"]:checked') as HTMLInputElement,
+  document.querySelector('#active') as HTMLInputElement,
+  document.querySelector('#addressHome') as HTMLInputElement,
+  document.querySelector('#addressWork') as HTMLInputElement
+];
+  
+  const [fullName, register, admin, active, addressHome, addressWork] = formFields;
+
+  let user: userType = {
+    fullName: fullName!.value,
+    register: register.value != '' ? register.value : undefined,
+    active: active.checked,
+    access: <accessOptions>admin.value,
+  };
+
+  content.innerHTML += <string>createLine(user, addressHome.value, addressWork.value);
 }
 
-function createLine(
-  fullName: string,
-  NrRegister: string | number,
-  admin: string,
-  active: boolean
-): string 
 
-{
+  /*--------------Arrow function--------------------------------*/ 
+  
 
- return  `
- <div class="card mb-1">
-   <div class="card-header">
-     ${NrRegister}
-   </div>
-   <div class="card-body">
-     <h5 class="card-title">${fullName}</h5>
-     <a href="#" class="btn btn-primary">${active ? 'Ativo' : 'Inativo'}</a>
-   </div>
-   <div class="card-footer bg-transparent border-success">
-     Acesso: ${admin}
-   </div>
- </div>`;
+
+
+
+accessOptionsValues.forEach((value: string, i: number) => {
+  accessRadio.innerHTML += `
+  <div class="form-check">
+  <input class="form-check-input " type="radio" name="access" id="accessRadio${i}" value="${value}">
+  <label class="form-check-label capitalLetter"  for="no">
+  ${value}
+  </label>
+  </div>`
+  });
+  
+
+
+  /*--------------Função declarativa--------------------------------*/ 
+
+  (<HTMLInputElement>document.getElementById('accessRadio0')).checked = true;
+
+  
+  function createLine(
+    {
+    fullName,
+    register = Math.random().toString(36).substring(7).toUpperCase(),
+    active= false,
+    access = accessOptions.undefined,
+  }: userType,
+    ...address: string[]
+   ): string {
+  return  `
+  <div class="card mb-1">
+      <div class="card-header">
+        ${register}
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">${fullName}</h5>
+        <a href="#" class="btn btn-primary">${active ? 'Ativo' : 'Inativo'}</a>
+      </div>
+      ${
+        address.length > 0
+          ? `<div class="card-body">
+          <h6 class="card-title">${address.join('<br/>')}</h6>
+        </div>`
+          : ''
+      }
+      <div class="card-footer bg-transparent border-success">
+        Acesso: ${access}
+      </div>
+    </div>`;
 }
-
-   
